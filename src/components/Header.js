0013e38ac15b5550b9ba1b4907e30734e2909d4e
iremumut <irem2000.nur@gmail.com>
2022-02-10 01:31:52 +0300

    context api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import ThemedButton from "./ThemedButton";
 import DarkModeToggle from "./DarkModeToggle";
 import defaultUser from "../data";
+import { UserContext } from "../context/user";
 
-function Header({ theme, setTheme, user, setUser }) {
+function Header() {
+  const { user, setUser,theme,setTheme } = useContext(UserContext);
+
   function handleLogin() {
     if (user) {
       setUser(null);
@@ -23,6 +26,6 @@ function Header({ theme, setTheme, user, setUser }) {
       </nav>
     </header>
   );
-}
+} 
 
 export default Header;
